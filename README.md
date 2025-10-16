bb79fd2d5a24cb3771d7a8dc84d932f59f9dcd1f
hasnaindar8 <hasnaindar1999@gmail.com>
2025-10-16 21:03:24 +0100

    complete seeding task

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+- You need to create two `.env` files to connect to your local databases, one for test environment and one for development environment.
+- File name should be: 
+  - `.env.test`
+  - `.env.development`
+- In each file add the following environment variable:
+  - For development: `PGDATABASE={your-database-name}`  
+  - For testing: `PGDATABASE={your-test-database-name}`
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,91 @@
-const db = require("../connection")
+const db = require("../connection");
+const { format } = require("node-pg-format");
+const { convertTimestampToDate } = require("../seeds/utils.js");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  try {
+    await db.query("DROP TABLE IF EXISTS comments;");
+    await db.query("DROP TABLE IF EXISTS articles;");
+    await db.query("DROP TABLE IF EXISTS topics;");
+    await db.query("DROP TABLE IF EXISTS users;");
+    await db.query(
+      "CREATE TABLE topics(slug varchar PRIMARY KEY, description varchar, img_url varchar(1000));"
+    );
+    await db.query(
+      "CREATE TABLE users(username varchar PRIMARY KEY, name varchar, avatar_url varchar(1000));"
+    );
+    await db.query(
+      "CREATE TABLE articles(article_id serial PRIMARY KEY, title varchar, topic varchar REFERENCES topics(slug), author varchar REFERENCES users(username), body text, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, votes INT DEFAULT 0, article_img_url varchar(1000));"
+    );
+    await db.query(
+      "CREATE TABLE comments(comment_id serial PRIMARY KEY, article_id INT REFERENCES articles(article_id), body text, votes INT DEFAULT 0, author varchar REFERENCES users(username), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
+    );
+    const topicFormattedData = topicData.map((topic) => {
+      return [topic.slug, topic.description, topic.img_url];
+    });
+    const insertTopicsQuery = format(
+      "INSERT INTO topics(slug, description, img_url) VALUES %L",
+      topicFormattedData
+    );
+    await db.query(insertTopicsQuery);
+
+    const userFormattedData = userData.map((user) => {
+      return [user.username, user.name, user.avatar_url];
+    });
+    const insertUsersQuery = format(
+      "INSERT INTO users(username, name, avatar_url) VALUES %L",
+      userFormattedData
+    );
+    await db.query(insertUsersQuery);
+
+    const articleFormattedData = articleData.map((article) => {
+      const updatedArticle = convertTimestampToDate(article);
+      return [
+        updatedArticle.title,
+        updatedArticle.topic,
+        updatedArticle.author,
+        updatedArticle.body,
+        updatedArticle.created_at,
+        updatedArticle.votes,
+        updatedArticle.article_img_url,
+      ];
+    });
+    const insertArticlesQuery = format(
+      "INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L",
+      articleFormattedData
+    );
+    await db.query(insertArticlesQuery);
+
+    const articleTitles = [
+      ...new Set(commentData.map((comment) => comment.article_title)),
+    ];
+    const articlesResponse = await db.query(
+      "SELECT article_id, title FROM articles WHERE title = ANY($1)",
+      [articleTitles]
+    );
+
+    const titlesObj = Object.fromEntries(
+      articlesResponse.rows.map((row) => [row.title, row.article_id])
+    );
+    const commentFormattedData = commentData.map((comment) => {
+      const article_id = titlesObj[comment.article_title];
+      const updatedcomment = convertTimestampToDate(comment);
+      return [
+        updatedcomment.body,
+        article_id,
+        updatedcomment.author,
+        updatedcomment.votes,
+        updatedcomment.created_at,
+      ];
+    });
+    const insertCommentsQuery = format(
+      "INSERT INTO comments(body, article_id, author, votes, created_at) VALUES %L",
+      commentFormattedData
+    );
+    await db.query(insertCommentsQuery);
+  } catch (error) {
+    console.log(`Error while seeding: ${error}`);
+  }
 };
+
 module.exports = seed;
