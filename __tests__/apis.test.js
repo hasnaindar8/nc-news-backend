af65944acc7e994aab78a20341c28056daa7ed54
hasnaindar8 <hasnaindar1999@gmail.com>
2025-10-22 17:01:07 +0100

    add test for GET /api/users endpoint

--- __tests__/apis.test.js
+++ __tests__/apis.test.js
@@ -39,3 +39,49 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/users", () => {
+  it("returns all users", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((res) => {
+        const body = res.body;
+        expect(body).toHaveProperty("users");
+        expect(body["users"]).toBeInstanceOf(Array);
+        expect(body["users"].length).toBe(4);
+        expect(body["users"][0]).toHaveProperty("username");
+        expect(body["users"][0]).toHaveProperty("name");
+        expect(body["users"][0]).toHaveProperty("avatar_url");
+        expect(body["users"]).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              username: "butter_bridge",
+              name: "jonny",
+              avatar_url:
+                "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+            }),
+            expect.objectContaining({
+              username: "icellusedkars",
+              name: "sam",
+              avatar_url:
+                "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
+            }),
+            expect.objectContaining({
+              username: "rogersop",
+              name: "paul",
+              avatar_url:
+                "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
+            }),
+            expect.objectContaining({
+              username: "lurker",
+              name: "do_nothing",
+              avatar_url:
+                "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+            }),
+          ])
+        );
+      });
+  });
+});
